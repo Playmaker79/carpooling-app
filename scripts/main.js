6967b6a8e27cbbc8defdb7b714e00060464e3637
arjun <arjunsreekumar@live.in>
2015-08-18 11:12:40 +0530

    Form validation and frontend

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  var menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-navigation-menu').removeClass("show");
+    $('.date_picker').pickadate({
+        selectYears: true
+    });
+    
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
+    });
+  });
+});
+
+
+
